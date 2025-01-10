9992fcbd11fec5fa0c3dad4b1a288d97750e0fd2
tadek <tadekpl@gmail.com>
2025-01-10 18:51:51 +0100

    add first simple tests

---
+++ src/scoreboard.js
@@ -0,0 +1,21 @@
+export class Scoreboard {
+  constructor() {
+    this.matches = [];
+  }
+
+  startMatch(homeTeam, awayTeam) {
+    // Implementation
+  }
+
+  updateScore(homeTeam, awayTeam, homeScore, awayScore) {
+    // Implementation
+  }
+
+  finishMatch(homeTeam, awayTeam) {
+    // Implementation
+  }
+
+  getSummary() {
+    // Implementation
+  }
+}
