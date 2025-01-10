13b50b1e196374ac2beaff87b18fd1aff57ed001
tadek <tadekpl@gmail.com>
2025-01-10 22:47:57 +0100

    improve tests by using jest.fakeTimers

--- tests/scoreboard.test.ts
+++ tests/scoreboard.test.ts
@@ -10,19 +10,38 @@ describe("Scoreboard", () => {
 
   test("should start a new match", () => {
     scoreboard.startMatch("Mexico", "Canada");
-    expect(scoreboard.getSummary().length).toBe(1);
+    expect(scoreboard.getSortedMatches().length).toBe(1);
   });
 
-  test("should update the score", () => {
-    const matchId = scoreboard.startMatch("Mexico", "Canada");
-    scoreboard.updateScore(matchId, 1, 0);
-    expect(scoreboard.getSummary()[0].homeScore).toBe(1);
+  describe("Finish a match", () => {
+    test("should finish a match", () => {
+      const matchId = scoreboard.startMatch("Mexico", "Canada");
+      scoreboard.finishMatch(matchId);
+      expect(scoreboard.getSortedMatches().length).toBe(0);
+    });
+
+    it("should throw an error if the match ID is not found", () => {
+      const invalidMatchId = "invalid-id";
+
+      expect(() => scoreboard.finishMatch(invalidMatchId)).toThrowError(
+        'Match with ID "invalid-id" not found'
+      );
+    });
   });
 
-  test("should finish a match", () => {
-    const matchId = scoreboard.startMatch("Mexico", "Canada");
-    scoreboard.finishMatch(matchId);
-    expect(scoreboard.getSummary().length).toBe(0);
+  describe("Update score", () => {
+    test("should update the score", () => {
+      const matchId = scoreboard.startMatch("Mexico", "Canada");
+      scoreboard.updateScore(matchId, 1, 0);
+      expect(scoreboard.getSortedMatches()[0].homeScore).toBe(1);
+    });
+
+    it("should throw an error if the match is not found", () => {
+      const invalidMatchId = "invalid-id";
+      expect(() => scoreboard.updateScore(invalidMatchId, 1, 2)).toThrow(
+        `Match with ID "${invalidMatchId}" not found for updating the score.`
+      );
+    });
   });
 
   describe("Match summary", () => {
@@ -33,22 +52,26 @@ describe("Scoreboard", () => {
       const match2Id = scoreboard.startMatch("Spain", "Brazil");
       scoreboard.updateScore(match2Id, 2, 2);
 
-      const summary = scoreboard.getSummary();
+      const summary = scoreboard.getSortedMatches();
       expect(summary[0].homeTeam).toBe("Spain");
     });
 
-    test("should return matches sorted by date if total score is equal", async () => {
+    test("should return matches sorted by date if total score is equal", () => {
+      jest.useFakeTimers();
+
       const match1Id = scoreboard.startMatch("Mexico", "Canada");
       scoreboard.updateScore(match1Id, 1, 0);
 
-      await wait();
+      jest.advanceTimersByTime(1000);
 
       const match2Id = scoreboard.startMatch("Spain", "Brazil");
       scoreboard.updateScore(match2Id, 0, 1);
 
-      const summary = scoreboard.getSummary();
+      const summary = scoreboard.getSortedMatches();
 
       expect(summary[0].homeTeam).toBe("Spain");
+
+      jest.useRealTimers();
     });
   });
 });
